a664ae2016138af560c0eabc8e0beb4bcc931993
israfil <israfil3019@gmail.com>
2021-08-06 16:07:39 +0300

    get data, display in index

--- src/weather_api/views.py
+++ src/weather_api/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
 from decouple import config
 import requests
+from .models import City
 from pprint import pprint
 
 def index(request):
+    cities = City.objects.all()
     url = config('BASE_URL')
-    city = "Berlin"
-    r = requests.get(url.format(city))
-    content = r.json()
-    pprint(content)
-    print(type(content))
-
-    return render(request, "weather_api/index.html")
+    city_data = []
+    for city in cities:
+        print(city)
+        r = requests.get(url.format(city))
+        content = r.json()
+        pprint(content)
+        data = {
+            'city': city.name,
+            'temperature': content['main']['temp'],
+            'description': content['weather'][0]['description'],
+            'icon': content['weather'][0]['icon'],
+        }
+        city_data.append(data)
+    print(city_data)
+    context = {
+        'city_data': city_data
+    }
+    return render(request, "weather_api/index.html", context)
