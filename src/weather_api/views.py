f14f8184aa2814c411d85c5c43101a48ff91c164
israfil <israfil3019@gmail.com>
2021-08-06 15:47:40 +0300

    add City model

--- src/weather_api/views.py
+++ src/weather_api/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from decouple import config
+import requests
+from pprint import pprint
 
 def index(request):
+    url = config('BASE_URL')
+    city = "Berlin"
+    r = requests.get(url.format(city))
+    content = r.json()
+    pprint(content)
+    print(type(content))
+
     return render(request, "weather_api/index.html")
