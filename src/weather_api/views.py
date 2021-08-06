26f7122e1fab8052064f52fc311ba25d068f7cc1
israfil <israfil3019@gmail.com>
2021-08-06 16:33:24 +0300

    add message, fix to add same city

--- src/weather_api/views.py
+++ src/weather_api/views.py
@@ -11,6 +11,16 @@ def index(request):
     form = CityForm(request.POST or None)
     cities = City.objects.all()
     url = config('BASE_URL')
+    if form.is_valid():
+        n_city = form.cleaned_data["name"]
+    if not City.objects.filter(name=n_city).exists():
+        r = requests.get(url.format(n_city))
+        if r.status_code == 200:
+            form.save()
+        else:
+            messages.warning(request, "City does not exists.")
+    else:
+        messages.warning(request, "City already exists.")
 
     city_data = []
     for city in cities:
